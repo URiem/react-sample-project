8937e6cbc04946b2a9dadf4358671cea225bd5bc
Ulrike Riemenschneider <uriemenschneider@gmail.com>
2023-11-15 12:44:33 +0100

    Add StatefulGreeting class to project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,20 @@
 // import logo from './logo.svg';
-import css from './App.module.css';
-import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps";
-import FunctionalGreeting from "./components/FunctionalGreeting";
-import Sidebar from "./components/Sidebar";
+// import css from './App.module.css';
+import './App.css';
+
+// import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps";
+// import FunctionalGreeting from "./components/FunctionalGreeting";
+// import Sidebar from "./components/Sidebar";
+import StatefulGreeting from "./components/StatefulGreeting";
 
 function App() {
   return (
-    <div className={css.App}>
+    // <div className={css.App}>
+    <div className="App">
       {/* <FunctionalGreeting /> */}
       {/* <FunctionalGreetingWithProps greeting="nice to meet you," name="Mike" age="32"/> */}
-      <Sidebar />
+      {/* <Sidebar /> */}
+      <StatefulGreeting greeting="I'm a stateful class component!" name="Mike"/>
     </div>
   );
 }
